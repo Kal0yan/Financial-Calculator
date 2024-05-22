df84af96c8e3e29010515cceaeecd2b3ab1d373a
Plamkata 22 <plamyk2006@gmail.com>
2024-05-22 19:10:22 +0300

    Create AppDbContext

---
+++ DataAccess Layer/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Business_Layer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess_Layer
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public AppDbContext() : base() { }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-AT94SBBO\\SQLEXPRESS;Database=CountriesDb11E2024;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
