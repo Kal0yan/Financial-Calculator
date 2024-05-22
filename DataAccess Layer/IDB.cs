767a02f99a3e59077be75db231659928d9c45f90
KSpasov <kspasov@infragistics.com>
2024-05-22 18:37:47 +0300

    Interface for CRUD made

---
+++ DataAccess Layer/IDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Business_Layer;
+using System.Threading.Tasks;
+
+
+namespace DataAccess_Layer
+{
+    public interface IDB <T, K>
+    {
+        Task Create(T entity);
+
+        Task<T> Read(K key, bool isReadOnly = true, bool useNavigationalProperties = false);
+
+        Task<List<T>> ReadAll(bool isReadOnly = true, bool useNavigationalProperties = false);
+
+        Task Update(T entity, bool useNavigationalProperties = false);
+
+        Task Delete(K key);
+    }
+}
