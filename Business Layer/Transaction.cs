2f6ca84e85ff57989244156af4a673ee8b477f78
KSpasov <kspasov@infragistics.com>
2024-05-22 18:29:38 +0300

    Add project files.

---
+++ Business Layer/Transaction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Business_Layer
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Category { get; set; }
+
+        [Required]
+        public float Amount { get; set; }
+
+
+        public string? Description { get; set; }
+
+        [Required]
+        public bool IncomeOrOutcome { get; set; }
+
+
+
+        private Transaction() { }
+        public Transaction(string category, float amount)
+        {
+            this.Category = category;
+            this.Amount = amount;
+
+        }
+        public Transaction(string category, float amount, string description)
+        {
+            this.Category = category;
+            this.Amount = amount;
+            this.Description = description;
+
+            if (amount > 0) IncomeOrOutcome = true;
+            else IncomeOrOutcome = false;
+        }
+        public Transaction(string category, float amount, bool incomeOrOutcome)
+        {
+            this.Category = category;
+            this.Amount = amount;
+            this.IncomeOrOutcome = incomeOrOutcome;
+        }
+    }
+}
---
+++ Business Layer/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Business_Layer
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+
+        private User() { }
+
+
+    }
+}
