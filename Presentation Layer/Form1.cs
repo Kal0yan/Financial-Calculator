b58067bddde2cc0484449be426544678acab13d3
Plamkata 22 <plamyk2006@gmail.com>
2024-06-06 22:12:09 +0300

    Created Presentatio Layer

---
+++ Presentation Layer/Form1.cs
@@ -0,0 +1,35 @@
+namespace Presentation_Layer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
